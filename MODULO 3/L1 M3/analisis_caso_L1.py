92eb65e121f3a60d189dbc4abf4c7515ab38540c
Joselyn Mena C <jmenac.08@gmail.com>
2026-02-09 20:49:29 -0300

    Estructura inicial curso Fundamentos del Análisis de Datos

---
+++ MODULO 3/L1 M3/analisis_caso_L1.py
@@ -0,0 +1,49 @@
+# Empleado 1
+nombre = "Cristina Pérez"
+edad = 45
+puesto = "Analista de Datos"
+
+anio_actual = 2026
+anios_restantes = 65 - edad
+anio_jubilacion = anio_actual + anios_restantes
+
+print("Empleado 1")
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Puesto:", puesto)
+print("Año estimado de jubilación:", anio_jubilacion)
+print("Resumen:", nombre, "-", puesto, "- Jubilación:", anio_jubilacion)
+print("")
+
+# Empleado 2
+nombre = "Cristián Soto"
+edad = 38
+puesto = "Ingeniero de Procesos"
+
+anio_actual = 2026
+anios_restantes = 65 - edad
+anio_jubilacion = anio_actual + anios_restantes
+
+print("Empleado 2")
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Puesto:", puesto)
+print("Año estimado de jubilación:", anio_jubilacion)
+print("Resumen:", nombre, "-", puesto, "- Jubilación:", anio_jubilacion)
+print("")
+
+# Empleado 3
+nombre = "María Castillo"
+edad = 29
+puesto = "Asistente de Análisis"
+
+anio_actual = 2026
+anios_restantes = 65 - edad
+anio_jubilacion = anio_actual + anios_restantes
+
+print("Empleado 3")
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Puesto:", puesto)
+print("Año estimado de jubilación:", anio_jubilacion)
+print("Resumen:", nombre, "-", puesto, "- Jubilación:", anio_jubilacion)
